fcfc85499d0205e296fad7074bbd2ca664f9478e
Kupuh <67791244+Kupuh@users.noreply.github.com>
2020-10-07 19:45:05 +0300

    Add files via upload

---
+++ Factorial/Factorial_Cal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial_Cal
+{
+    public class Factorial
+    {
+        public static double FuncFactorial(double x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                double result = x * FuncFactorial(x - 1);
+                return result;
+            }
+        }
+
+    }
+}
