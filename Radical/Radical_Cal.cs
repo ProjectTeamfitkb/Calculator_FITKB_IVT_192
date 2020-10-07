342c35cea90b71a7c8fa6bd2b05c4e67dbde5b4e
lunarythorn <60178774+lunarythorn@users.noreply.github.com>
2020-10-07 20:05:28 +0300

    Add files via upload

---
+++ Radical/Radical_Cal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Radical_Cal
+{ 
+    public static class Radical
+    { 
+        public static double FuncRadical(double chislo, double stepen)
+        {
+            double result = Math.Pow(chislo, 1 / stepen);
+            return result;
+        }
+    }
+}
