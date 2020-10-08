93f4287955ccbd840aa7bab320ae062607e7d524
ksenyapopova2001 <67839241+ksenyapopova2001@users.noreply.github.com>
2020-10-08 14:27:44 +0300

    Add files via upload

---
+++ LibM/Minus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LibM
+{
+    public class Minus
+    {
+        public static double Min(double x, double y)
+        {
+            double result = x - y;
+            return result;
+        }
+    }
+}
---
+++ Minus/Minus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Minus
+{
+    public class Minus
+
+    {
+
+            
+    }
+    public class Plus
+
+    {
+        public static double Mins(double x, double y)
+        {
+            double result = x - y;
+            return result;
+        }
+
+    }
+
+}
