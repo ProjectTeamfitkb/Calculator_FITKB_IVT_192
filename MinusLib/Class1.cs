438b5742e627b156c55ba21192b409b759f89797
ksenyapopova2001 <67839241+ksenyapopova2001@users.noreply.github.com>
2020-10-21 14:28:06 +0300

    Add files via upload

---
+++ MinusLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MinusLib
+{
+    
+   
+        public static class Class1
+        {
+            public static double Min(double x, double y)
+            {
+                double result = x - y;
+                return result;
+            }
+        }
+    
+}
