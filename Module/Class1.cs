5ff939578801edcc839146d88fecb6f60f6cd4d7
Dima98772 <67924007+Dima98772@users.noreply.github.com>
2020-10-21 13:02:00 +0300

    Add files via upload

---
+++ Module/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class Class1
+    {
+        public static double Module(double variable_first)
+        { if (variable_first > 0)
+                return variable_first;
+            else return variable_first * -1;
+        }
+    }
+}
