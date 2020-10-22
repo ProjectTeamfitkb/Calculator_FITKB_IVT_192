b45f3094e597ead20fd301df1846621f96d6b36e
Fomin.Alexey <lekha.fomin2013@yandex.ru>
2020-10-22 22:08:54 +0300

    Program localization

---
+++ SelectLanguage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for SelectLanguage.xaml
+    /// </summary>
+    public partial class SelectLanguage : Window
+    {
+        public SelectLanguage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().ShowDialog();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmd.SelectedIndex == 0)
+                Properties.Settings.Default.languageCode = "ru-RU";
+            else
+                Properties.Settings.Default.languageCode = "en-US";
+            Properties.Settings.Default.Save();
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
+
+        
+    }
+}
